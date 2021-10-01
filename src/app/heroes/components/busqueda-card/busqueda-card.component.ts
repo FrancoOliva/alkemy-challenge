43c8290ccdc5f9d008ab2ba1c3dd64f145cfcfc1
Franco Oliva <franco.oliva1991@gmail.com>
2021-10-01 09:46:58 -0300

    Podemos validar y guardar equipo en localStorage

--- src/app/heroes/components/busqueda-card/busqueda-card.component.ts
+++ src/app/heroes/components/busqueda-card/busqueda-card.component.ts
@@ -8,20 +8,70 @@ import { Result } from '../../interfaces/response.interface';
 })
 export class BusquedaCardComponent implements OnInit {
 
-  @Input() heroe!: Result;
+
+  @Input() resultadoBusqueda!: Result[];
 
   equipo: Result[] = [];
 
-  constructor( ) { }
+  buenos: number = 0;
+  malos: number = 0;
+
+  constructor( ) { 
+    
+    if( localStorage.getItem('equipo') ){      
+
+      this.equipo = JSON.parse(localStorage.getItem('equipo')!);
+
+    } else {
+      this.equipo = [];
+    }
+  }
 
   ngOnInit(): void {
+
+    
     
   }
 
   agregar(heroe: Result){
-    console.log('Agregar al equipo');
-    console.log(heroe);
-    console.log(this.equipo.length);
+    
+
+    // Validaciones
+    // Max 6 jugadores: 3 buenos 3 malos
+    if( this.equipo.length < 6){
+
+      if(heroe.biography.alignment == "good"){
+
+        if( this.buenos < 3){
+          this.buenos++;
+          this.equipo.push(heroe);
+
+        } else {
+          console.log('Solo se permiten 3 héroes buenos por equipo.');
+        }
+
+        
+      } else if(heroe.biography.alignment == "bad"){
+
+        if( this.malos < 3){
+
+          this.malos++;
+          this.equipo.push(heroe);
+    
+        } else {
+          console.log('Solo se permiten 3 héroes malos por equipo.');
+        }  
+        
+      }        
+        
+    } else {
+      console.log('Solo se permiten 6 héroes por equipo.');
+    }
+
+
+    localStorage.setItem('equipo', JSON.stringify(this.equipo) );
+    
+    
   }
 
 
